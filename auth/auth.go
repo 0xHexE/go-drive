b2f00bec4ea264f66dba42bd13065e2b8c47dfda
Omkar Yadav <httpsomkar@gmail.com>
2019-12-28 19:36:39 +0400

    added rules

--- auth/auth.go
+++ auth/auth.go
@@ -5,3 +5,31 @@
  */
 
 package auth
+
+import "gopkg.in/yaml.v2"
+
+type Config struct {
+	ApiVersion, Kind string
+	Metadata         ConfigMetadata
+	Spec             ConfigSpec
+}
+
+type ConfigSpec struct {
+	Match  string
+	Access ConfigAccess
+}
+
+type ConfigAccess struct {
+	Read, Write, Delete string
+}
+
+type ConfigMetadata struct {
+	Name, Namespace string
+}
+
+// Parse config from yaml
+func ParseConfig(input []byte) (error, *Config) {
+	var config Config
+	err := yaml.Unmarshal(input, &config)
+	return err, &config
+}
